d1c10e9acf734a472bbf235bed1b06b0d13bf1ed
Santosh_Chappidi <santoshchappidi03@gmail.com>
2024-07-31 21:36:31 +0530

    updates

--- task-management/config/index.tsx
+++ task-management/config/index.tsx
@@ -2,8 +2,8 @@ import axios from "axios";
 
 
 const api = axios.create({
-    // baseURL: "https://task-management-nextjs-dpco.onrender.com",
-    baseURL: "http://localhost:8000",
+    baseURL: "https://task-management-nextjs-dpco.onrender.com",
+    // baseURL: "http://localhost:8000",
 })
 
 export default api
